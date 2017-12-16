d7ee6c4cbf490e9fbed443754c19e78045eb6dd5
Thomas <thomas.sypniewski@gmail.com>
2017-12-16 19:57:34 +0100

    add true bulgarian alphabet in datas

--- src/components/alphabet.js
+++ src/components/alphabet.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default class Alphabet extends React.Component {
 	render() {
+		console.log(this.props.letter);
 		return(
 			<div className="alphabet">
-				{this.props.letter}
+				{this.props.letter.uppercase}
 			</div>
 		)
 	}
