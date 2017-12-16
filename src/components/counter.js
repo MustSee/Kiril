0cad9ba9f20c0aa76d71901019e5ef5c101c1919
Thomas <thomas.sypniewski@gmail.com>
2017-12-16 13:58:40 +0100

    add screenfull module, for fullScreen experience on mobile

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import screenfull from 'screenfull';
 
 export default class Counter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleFullScreen = this.handleFullScreen.bind(this);
+	}
+
+	handleFullScreen() {
+		console.log('handleFullScreen');
+		console.log(screenfull);
+		if(screenfull.enabled) {
+			screenfull.request();
+		}
+	}
+
 	render() {
 		return(
-			<div className='counter'>
-				{this.props.counter + 1} / {this.props.total}
+			<div className='counter' onClick={this.handleFullScreen}>
+					{this.props.counter + 1} / {this.props.total}
 			</div>
 		)
 	}
