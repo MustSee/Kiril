8dbb4a67d232565fa3ff64f0434f6b05c9077400
Thomas <thomas.sypniewski@gmail.com>
2018-03-03 21:59:26 +0100

    routing bug

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Alphabet from './alphabet';
 import Home from './home';
 import Games from './games';
@@ -8,14 +8,12 @@ import Culture from './culture';
 export default class Main extends React.Component {
   render() {
     return (
-      <HashRouter>
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/alphabet' component={Alphabet} />
           <Route path='/games' component={Games} />
           <Route path='/culture' component={Culture} />
         </Switch>
-      </HashRouter>
     );
   }
 }
