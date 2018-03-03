a5680b6f419840fb790a65c295d29b73104d0734
Thomas <thomas.sypniewski@gmail.com>
2018-03-03 23:09:46 +0100

    router fix

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Alphabet from './alphabet';
 import Home from './home';
 import Games from './games';
@@ -8,12 +8,15 @@ import Culture from './culture';
 export default class Main extends React.Component {
   render() {
     return (
+      // Il faut mettre le HashRouter à ce niveau pour que la page fonctionne sur MustSee.github.io
+      <HashRouter>
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/alphabet' component={Alphabet} />
           <Route path='/games' component={Games} />
           <Route path='/culture' component={Culture} />
         </Switch>
+      </HashRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
+// import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
 ReactDOM.render((
@@ -9,3 +10,4 @@ ReactDOM.render((
 		<App />
 	</HashRouter>
 ), document.getElementById('root'));
+// registerServiceWorker();
